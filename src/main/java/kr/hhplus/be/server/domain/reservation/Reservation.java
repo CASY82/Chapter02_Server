1a78dfbc24e770f5d7d7b276440702ab0a16f483
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-04-11 06:13:31 +0900

    add : 예약 커밋 빼먹은것 추가

--- src/main/java/kr/hhplus/be/server/domain/reservation/Reservation.java
+++ src/main/java/kr/hhplus/be/server/domain/reservation/Reservation.java
@@ -2,13 +2,21 @@ package kr.hhplus.be.server.domain.reservation;
 
 import java.time.Instant;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reservation {
-	private Long id; // DB 식별자
-	private Long reservationId; // 도메인 식별자
+	private Long id;
+	private Long reservationId;
 	private Long userRefId;
 	private Long orderRefId;
 	private Long seatRefId;
 	private Long scheduleRefId;
+	private String reserveStatus;
 	private Instant createdAt;
 	private Instant updatedAt;
 }
--- src/main/java/kr/hhplus/be/server/presentation/api/v1/reserve/ReservationController.java
+++ src/main/java/kr/hhplus/be/server/presentation/api/v1/reserve/ReservationController.java
@@ -1,8 +1,9 @@
 package kr.hhplus.be.server.presentation.api.v1.reserve;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,37 +11,76 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.hhplus.be.server.application.facade.ReservationFacade;
+import kr.hhplus.be.server.domain.schedule.Schedule;
+import kr.hhplus.be.server.domain.schedule.ScheduleService;
+import kr.hhplus.be.server.domain.seat.Seat;
+import kr.hhplus.be.server.domain.seat.SeatService;
+import kr.hhplus.be.server.presentation.api.v1.obj.ReservationRequest;
+import kr.hhplus.be.server.presentation.api.v1.obj.ScheduleResponse;
+import kr.hhplus.be.server.presentation.api.v1.obj.SeatResponse;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class ReservationController {
 	
+	private final SeatService seatService;
+	private final ScheduleService scheduleService;
+	private final ReservationFacade reservationFacade;
+	
+	/**
+	 * 예약 가능한 날짜 선택 후, 예약 가능한 좌석
+	 * @param scheduleRefId
+	 * @return
+	 */
 	@GetMapping("/reservations/available/seat")
-	public ResponseEntity<List<Integer>> getAvailableSeat(@RequestParam("venueId") int venueId) {
-		List<Integer> seatList = new ArrayList<>();
+	public ResponseEntity<SeatResponse> getAvailableSeat(@RequestParam("scheduleRefId") long scheduleRefId) {
+		if (scheduleRefId < 0) return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(null);
 		
-		seatList.add(1);
-		seatList.add(2);
-		seatList.add(3);
-		seatList.add(4);
-		seatList.add(5);
+		List<Seat> seatList = this.seatService.getAvailableSeatList(scheduleRefId);
 		
-		return ResponseEntity.ok(seatList);
+		List<Long> seatIds = seatList.stream()
+                .map(Seat::getSeatId)
+                .collect(Collectors.toList());
+
+        // SeatResponse 객체 생성 및 seatIds 설정
+        SeatResponse seatResponse = new SeatResponse();
+        seatResponse.setSeatIds(seatIds);
+		
+		return ResponseEntity.ok(seatResponse);
 	}
 	
+	/**
+	 * 예약 가능한 날짜 목록 조회
+	 * 요구 사항 외 도메인의 식별자 값들은 외부에서 받는다는 전제로 구현
+	 * @param performanceId
+	 * @param venueId
+	 * @return
+	 */
 	@GetMapping("/reservations/available/schedule")
-	public ResponseEntity<List<Integer>> getAvailableSchedule(@RequestParam("performanceId") int performanceId) {
-		List<Integer> scheduleList = new ArrayList<>();
+	public ResponseEntity<ScheduleResponse> getAvailableSchedule(@RequestParam("performanceId") long performanceId,
+															     @RequestParam("venueId") long venueId) {
 		
-		scheduleList.add(1);
-		scheduleList.add(2);
-		scheduleList.add(3);
-		scheduleList.add(4);
-		scheduleList.add(5);
+		if (performanceId < 0) return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(null);
+		if (venueId < 0) return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(null);
 		
-		return ResponseEntity.ok(scheduleList);
+		List<Schedule> scheduleList = this.scheduleService.getScheduleList(performanceId, venueId);
+		
+		List<String> dateList = scheduleList.stream()
+				.map(Schedule::getDate)
+				.collect(Collectors.toList());
+		
+		ScheduleResponse scheduleResponse = new ScheduleResponse();
+		scheduleResponse.setDateList(dateList);
+		
+		return ResponseEntity.ok(scheduleResponse);
 	}
 	
 	@PostMapping("/reservations")
-	public ResponseEntity<Boolean> reserve(@RequestBody int performanceId) {
+	public ResponseEntity<Boolean> reserve(@RequestBody ReservationRequest request) {
+		this.reservationFacade.reservation(request.getScheduleId(), request.getSeatId(), request.getUserId());
+		
 		return ResponseEntity.ok(true);
 	}
 	
