fe92b050b8024bbecc088bae646ff91b20456525
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-04-11 06:15:38 +0900

    add : 날짜 도메인 추가

--- src/main/java/kr/hhplus/be/server/domain/schedule/Schedule.java
+++ src/main/java/kr/hhplus/be/server/domain/schedule/Schedule.java
@@ -2,13 +2,20 @@ package kr.hhplus.be.server.domain.schedule;
 
 import java.time.Instant;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Schedule {
 	private Long id;
 	private Long scheduleId;
 	private Long performanceRefId;
 	private Long venueRefId;
-	private Integer availableSeats;
-	private Instant scheduleDateTime;
+	private String date;
 	private Instant createdAt;
 	private Instant updatedAt;
+
 }
